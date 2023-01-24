1ce857c5f5d203047c9e29a107ad089cc6e41468
Helmar <helmar.israel@g.msuiit.edu.ph>
2023-01-25 00:27:42 +0800

    Adjust UI positioning

--- game/widget.py
+++ game/widget.py
@@ -5,33 +5,22 @@ import pygame
 def create_button(screen, img, rect, type, cursor='arrow'):
     btn = Button()
     w,h = rect.width, rect.height
-    if type == 'logo':
-        rect.x = 10
-        rect.y = (NAV_HEIGHT - h) / 2
 
     if type == 'shuffle':
         rect.x = ((WIDTH - w) / 2) - 100
-        rect.y = HEIGHT - (NAV_HEIGHT + 100) - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -400)
+        rect.y = HEIGHT - (NAV_HEIGHT + 100) - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -650)
 
     if type == 'solve':
         rect.x = ((WIDTH - w) / 2) + 110
-        rect.y = HEIGHT - (NAV_HEIGHT + 100) - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -400)
-
-    if type == 'help-btn':
-        rect.x = WIDTH - w - 20
-        rect.y = HEIGHT - h - 20
-
-    if type == 'help-text':
-        rect.x = int((WIDTH - w) / 2)
-        rect.y = int(HEIGHT - (NAV_HEIGHT*2))
+        rect.y = HEIGHT - (NAV_HEIGHT + 100) - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -650)
 
     if type == 'bfs':
         rect.x = int((WIDTH - w - 130) / 2)
-        rect.y = HEIGHT - NAV_HEIGHT - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -150)
+        rect.y = HEIGHT - NAV_HEIGHT - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -400)
 
     if type == 'astar':
         rect.x = int((WIDTH - w + 200) / 2)
-        rect.y = HEIGHT - NAV_HEIGHT - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -150)
+        rect.y = HEIGHT - NAV_HEIGHT - (TILESIZE*GAME_SIZE) - (TOP_MARGIN * -400)
 
     btn.draw_img(screen, img, rect)
  
